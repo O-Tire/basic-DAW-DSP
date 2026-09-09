d3bbbca2e2ff831a3ece75041c41e2ca8f36ca3e
O-Tire <164386722+O-Tire@users.noreply.github.com>
2026-09-09 09:10:16 +0000

    Fully installed miniaudio.

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,24 @@
-#include <cstdio>
-#include "miniaudio/miniaudio.h"
+#define MINIAUDIO_IMPLEMENTATION
+#include "libraries/miniaudio.h"
+
+#include <stdio.h>
 
 int main()
 {
-    printf("Hello World!");
+    ma_result result;
+    ma_engine engine;
+
+    result = ma_engine_init(NULL, &engine);
+    if (result != MA_SUCCESS) {
+        return -1;
+    }
+
+    ma_engine_play_sound(&engine, "D:/Music/Heat.mp3", NULL);
+
+    printf("Press Enter to quit...");
+    getchar();
+
+    ma_engine_uninit(&engine);
 
     return 0;
 }
