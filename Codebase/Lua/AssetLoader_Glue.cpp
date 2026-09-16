8b263865347ef11fd0c38f9c676b1b122cbe801b
O-Tire <164386722+O-Tire@users.noreply.github.com>
2026-09-16 05:58:35 +0000

    Added AssetLoader_Glue.cpp

---
+++ Codebase/Lua/AssetLoader_Glue.cpp
@@ -0,0 +1,29 @@
+#include "AssetLoader.hpp"
+
+extern "C"
+{
+    #include "lua.h"
+    #include "lauxlib.h"
+}
+
+static int LoadWAV_lua(lua_State* L)
+{
+    AssetLoader* AL = static_cast<AssetLoader*>(lua_touserdata(L, lua_upvalueindex(1)));
+    const char* path = luaL_checkstring(L, 1);
+    
+    AL->LoadWAV(path);
+    
+    lua_pushinteger(L, AL->Clips.size() - 1);
+    return 1;
+}
+
+static int LoadMIDI_lua(lua_State* L)
+{
+    AssetLoader* AL = static_cast<AssetLoader*>(lua_touserdata(L, lua_upvalueindex(1)));
+    const char* path = luaL_checkstring(L, 1);
+    
+    AL->LoadMIDI(path);
+    
+    lua_pushinteger(L, AL->MIDIs.size() - 1);
+    return 1;
+}
--- Codebase/Lua/Project.cpp
+++ Codebase/Lua/Project.cpp
@@ -14,11 +14,25 @@ extern "C"
     #include "lualib.h"
 }
 
+// Paste in Glue CPPs.
+#include "AssetLoader_Glue.cpp"
 
 void Project::Run(AudioEngine* AE, AssetLoader* AL, Sequencer* SE, DAW* DA, std::string path)
 {
     lua_State* L = luaL_newstate();
     luaL_openlibs(L);
+    
+    //------------- Register.
+    
+    // LoadWAV()
+    lua_pushlightuserdata(L, AL);
+    lua_pushcclosure(L, LoadWAV_lua, 1);
+    lua_setglobal(L, "load_wav");
+    
+    // LoadMIDI()
+    lua_pushlightuserdata(L, AL);
+    lua_pushcclosure(L, LoadMIDI_lua, 1);
+    lua_setglobal(L, "load_midi");
     
     //------------- Execute script.
     
